72a1b39ebd14dac273480db06fd7c8a9802e9cc7
MoosiestMoose <lnpow@yahoo.com>
2022-12-19 14:45:48 +0000

    Add project files.

---
+++ Command.cs
@@ -0,0 +1,40 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+namespace _3DR_R23_Add_in_Template1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+
+            // Modify document within a transaction
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Transaction Name");
+                tx.Commit();
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DR_R23_Add_in_Template1
+{
+    internal static class Utils
+    {
+        internal static double Metric(double meters)
+        {
+            double feet = meters * 3.28084;
+
+            return feet;
+        }
+    }
+}
